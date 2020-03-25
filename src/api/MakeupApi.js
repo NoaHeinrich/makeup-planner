f4e182530deb2bebb556afbae4c372f2b9fbdcf5
Noa Heinrich <noawriteplay@gmail.com>
2020-03-25 09:09:00 -0500

    Nevermind, the problem was me

--- src/api/MakeupApi.js
+++ src/api/MakeupApi.js
@@ -1,6 +1,6 @@
-const URL = "http://makeup-api.herokuapp.com/api/v1/products.json"
+const URL = `http://makeup-api.herokuapp.com/api/v1/products.json`
 
 const fetchMakeupByBrandName = brandName => {
-  return fetch(URL + "?brand=" + brandName)
+  return fetch(`${URL}?brand=${brandName}`)
   .then(resp => resp.json())
 }
